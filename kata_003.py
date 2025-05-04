42d357b769f90a5712230c24c7c87c8175d156a5
ANdrey Baranov <randebar@gmail.com>
2025-05-04 16:47:46 +0300

    kata_003

---
+++ kata_003.py
@@ -0,0 +1,5 @@
+#Write a function that accepts a non-negative integer n and a string s as parameters,
+# and returns a string of s repeated exactly n times.
+
+def repeat_str(repeat:int, string: str) -> str:
+    return string*repeat
